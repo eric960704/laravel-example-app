d6b2ec51201f15100606f5df22d0faf8892b1d2e
eric960704 <eric0930885321@gmail.com>
2024-05-10 17:37:26 +0800

    get weather

--- app/Http/Controllers/ExperimentFunction/TestController.php
+++ app/Http/Controllers/ExperimentFunction/TestController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers\ExperimentFunction;
 
 use App\Http\Controllers\Controller;
 use App\Jobs\ProcessPodcast;
+use Illuminate\Http\Request;
 use Telegram\Bot\Laravel\Facades\Telegram;
 use Telegram\Bot\Api;
+use Illuminate\Support\Facades\Http;
 
 class TestController extends Controller
 {
@@ -26,5 +28,15 @@ class TestController extends Controller
         ]);
 
         return $response;
+    }
+
+    public function getWeather(Request $request)
+    {
+        $url = config('centralWeather.api.url');
+        $url = $url . '?Authorization=' . config('centralWeather.api.authorization') . '&limit=' . $request->limit . '&locationName=' . urlencode($request->location);
+        $response = Http::get($url);
+        $response->json();
+
+        return $response->json();
     }
 }
---
+++ config/centralWeather.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'api' => [
+        'url' => 'https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001',
+        'authorization' => env('METEOROLOGICAL_OPENDATA_PLATFORM_MEMBERSHIP_AUTHORIZATION_CODE')
+    ]
+];
